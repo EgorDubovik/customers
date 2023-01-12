eaf5fc6ef037cee53e7df0ed8dc6dc23512fe95f
EgorDubovik <posik.web.m@gmail.com>
2023-01-11 21:45:32 -0600

    view customer information

--- resources/views/customer/show.blade.php
+++ resources/views/customer/show.blade.php
@@ -14,7 +14,20 @@
                 <div class="card">
                     <div class="card-header">Customer information</div>
                     <div class="card-body">
-
+                        <p>
+                            <span class="text-muted">Customer name:</span>
+                            <span style="margin-left: 15px;">{{$customer->name}}</span>
+                        </p>
+                        <p>
+                            <span class="fs-14 fw-bold">{{$customer->address->full()}}</span>
+                            <i class="fe fe-copy pull-right text-secondary" style="cursor: pointer"></i>
+                            <a href="#"> <i class="fe fe-map-pin pull-right" style="margin-right: 10px;"></i></a>
+                        </p>
+                        <p>
+                            <span class="fs-14 text-info fw-bold">{{$customer->phone}}</span>
+                            <i class="fe fe-copy pull-right text-secondary" onclick="copy_to({{$customer->phone}})" style="cursor: pointer"></i>
+                            <a href="#"> <i class="fe fe-phone-call pull-right" style="margin-right: 10px;"></i></a>
+                        </p>
                     </div>
 
                 </div>
@@ -23,10 +36,41 @@
                 <div class="card">
                     <div class="card-header">Notes history</div>
                     <div class="card-body">
+                        <ul class="task-list">
+                            <li class="d-sm-flex">
+                                <div>
+                                    <i class="task-icon bg-primary"></i>
+                                    <h6 class="fw-semibold"><span class="fs-14 mx-2 fw-normal">09 July 2021</span></h6>
+                                    <p class="text-muted fs-15">Adam Berry finished task on</p>
+                                </div>
+                            </li>
+                            <li class="d-sm-flex">
+                                <div>
+                                    <i class="task-icon bg-primary"></i>
+                                    <h6 class="fw-semibold"><span class="fs-14 mx-2 fw-normal">09 July 2021</span></h6>
+                                    <p class="text-muted fs-15">Adam Berry finished task on</p>
+                                </div>
+                            </li>
+                            <li class="d-sm-flex">
+                                <div>
+                                    <i class="task-icon bg-primary"></i>
+                                    <h6 class="fw-semibold"><span class="fs-14 mx-2 fw-normal">09 July 2021</span></h6>
+                                    <p class="text-muted fs-15">Adam Berry finished task on</p>
+                                </div>
+                            </li>
 
+                        </ul>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 @stop
+
+@section('scripts')
+    <script>
+        function copy_to(text){
+            navigator.clipboard.writeText(text);
+        }
+    </script>
+@endsection
