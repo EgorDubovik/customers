001862143c43376b996fc8d695957e8c13bb9d40
EgorDubovik <posik.web.m@gmail.com>
2023-03-23 08:55:17 -0500

    fixes

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -57,7 +57,7 @@
                             <div class="social social-profile-buttons me-2">
                                 <a class="social-icon text-primary" href="javascript:void(0)"><i class="fe fe-map"></i></a>
                             </div>
-                            @if (Auth::user()->company->address->full)
+                            @if (Auth::user()->company->address)
                                 <span class="my-auto">{{Auth::user()->company->address->full}}</span>
                             @endif
                         </li>
