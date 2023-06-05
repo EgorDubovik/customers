b2bbc4e0a1671debfeb8d2ce377fc9008650739e
EgorDubovik <posik.web.m@gmail.com>
2023-06-05 08:55:43 -0500

    create and replace calendarPlugin

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.main')
 
+@section('css')
+    <link href="{{ URL::asset('assets/css/myCalendar.css')}}" rel="stylesheet" />
+@endsection
+
 @section('content')
 <div class="main-container container-fluid">
     <!-- ROW-1 OPEN -->
@@ -25,46 +29,69 @@
 @stop
 
 @section('scripts')
-<script src="{{ URL::asset('assets/plugins/fullcalendar/moment.min.js')}}"></script>
-<script src="{{ URL::asset('assets/plugins/fullcalendar/fullcalendar.min.js')}}"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+<script src="{{ URL::asset('assets/js/appointmentScheduler.js')}}"></script>
     <script>
         $(document).ready(function(){
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                slotMinTime: "07:00:00",
-                slotMaxTime: "21:00:00",
-                contentHeight: 'auto',
-                height:500,
-                displayEventEnd: true,
-                headerToolbar: {
-                    left: 'prev,next today',
-                    center: 'title',
-                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
-                },
-                events: [
+            $('#calendar').appointmentScheduler({
+                appointments : [
                     @foreach ($appointments as $appointment)
                         {
                             title: '{{ $appointment->customer->name }}', 
-                            start: '{{ $appointment->start }}', 
-                            end: '{{ $appointment->end }}',
-                            color: "#1565C0",
-                            url: "{{ route('appointment.show',['appointment'=>$appointment]) }}",
-                            
+                            startTime: moment('{{ $appointment->start }}'), 
+                            endTime: moment('{{ $appointment->end }}'),
+                            background: "#1565C0",
+                            href: "{{ route('appointment.show',['appointment'=>$appointment]) }}",
+                            status : 'pending',
                         },    
-                    @endforeach
-                ],
-                editable: true,
-                eventClick: function(info) {
-                    if (info.event.url) {
-                        event.jsEvent.preventDefault();
-                        window.open(info.event.url);
-                        
-                    }
-                }
+                    @endforeach    
                 
+                    // { startTime: moment('2023-06-01 09:00'),endTime:moment('2023-06-01 13:00') , title: 'Meeting 1', background : '#1565c0', status : 'pending'},
+                    // { startTime: moment('2023-06-01 11:00'),endTime:moment('2023-06-01 13:00') , title: 'Meeting 2', background : '#1565c0', status : 'pending' },
+                    // { startTime: moment('2023-06-01 13:00'),endTime:moment('2023-06-01 15:00') , title: 'Meeting 3', background : '#1565c0', status : 'pending' },
+                    // { startTime: moment('2023-05-13 13:00'),endTime:moment('2023-05-13 15:00') , title: 'Meeting 4', background : '#1565c0', status : 'pending' },
+                    // { startTime: moment('2023-06-04 13:00'),endTime:moment('2023-06-04 15:00') , title: 'Meeting 5', background : '#1565c0', status : 'pending' },
+                    // { startTime: moment('2023-06-10 9:00'),endTime:moment('2023-06-10 11:00') , title: 'Meeting 7', background : '#1565c0', status : 'pending' },
+                    // { startTime: moment('2023-06-05 13:00'),endTime:moment('2023-06-05 15:00') , title: 'Meeting 6', href : "?test", background : '#46c015', status : 'done' },
+                ],
                 
             });
-            calendar.render();
+            // var calendarEl = document.getElementById('calendar');
+            // var calendar = new FullCalendar.Calendar(calendarEl, {
+            //     slotMinTime: "07:00:00",
+            //     slotMaxTime: "21:00:00",
+            //     contentHeight: 'auto',
+            //     height:500,
+            //     displayEventEnd: true,
+            //     headerToolbar: {
+            //         left: 'prev,next today',
+            //         center: 'title',
+            //         right: 'dayGridMonth,timeGridWeek,timeGridDay'
+            //     },
+            //     events: [
+            //         @foreach ($appointments as $appointment)
+            //             {
+            //                 title: '{{ $appointment->customer->name }}', 
+            //                 start: '{{ $appointment->start }}', 
+            //                 end: '{{ $appointment->end }}',
+            //                 color: "#1565C0",
+            //                 url: "{{ route('appointment.show',['appointment'=>$appointment]) }}",
+                            
+            //             },    
+            //         @endforeach
+            //     ],
+            //     editable: true,
+            //     eventClick: function(info) {
+            //         if (info.event.url) {
+            //             event.jsEvent.preventDefault();
+            //             window.open(info.event.url);
+                        
+            //         }
+            //     }
+                
+                
+            // });
+            // calendar.render();
         });
       </script>
 @endsection
