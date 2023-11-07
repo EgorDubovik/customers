c2f4d3fe63c0bbb5d2c1262d9bd281264a3258fb
EgorDubovik <posik.web.m@gmail.com>
2023-11-06 19:01:49 -0600

    small update for online booking

--- resources/views/book-appointment/index.blade.php
+++ resources/views/book-appointment/index.blade.php
@@ -45,7 +45,7 @@
                <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab" tabindex="0">
                   <div class="row">
                      <div class="col-md-3 mb-2 d-none d-sm-block">
-                        <ul class="list-group" id="service-list">
+                        <ul class="list-group list-group-flush" id="service-list">
                            
                         </ul>
                      </div>
@@ -54,10 +54,16 @@
                            @foreach ($company->services as $service)
                               <div class="col-md-3">
                                  <div class="card card-box" data-check = "false" data-service-title='{{ $service->title }}'  data-service-id="{{ $service->id }}" onclick="checkService(this)">
-                                    <div class="card-header">
-                                       {{ $service->title }}
+                                    <div class="card-header service-head">
+                                       <div class="card-title mb-0">{{ $service->title }} </div>
+                                       <div class="cont-icon">
+                                          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                                             <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                             <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+                                          </svg>
+                                       </div>
                                     </div>
-                                    <div class="card-body">
+                                    <div class="card-body pb-1">
                                        <p class="service-description">{{ $service->description }}</p>
                                        <p class="service-price">${{ $service->price }}</p>
                                     </div>
