542ab487d1e7e41aed24e8d6dc1fc242d8fa68ec
EgorDubovik <posik.web.m@gmail.com>
2024-02-02 23:19:38 -0600

    bug fixes

--- resources/views/customer/layout/referral-block.blade.php
+++ resources/views/customer/layout/referral-block.blade.php
@@ -1,4 +1,4 @@
-@if (Auth::user()->company->companySettings->referal_enable)
+@if (Auth::user()->company->companySettings->referral_enable)
       <div class="card-footer">
          <p>Referral stat: <span class="text-success">{{ count($customer->referralStat) }}/{{ $referral_count }} -> ${{ $referral_discount }}</span></p>
          <p>
