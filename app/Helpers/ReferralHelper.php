afb4d5c5a5434a1134a43b8fe44981737d749bcb
EgorDubovik <posik.web.m@gmail.com>
2024-02-18 19:15:36 -0600

    fixes

--- app/Helpers/ReferralHelper.php
+++ app/Helpers/ReferralHelper.php
@@ -8,11 +8,6 @@ if (! function_exists('get_referral_info')) {
     {
         $referal_count = 0;
         $referal_discount = 0;
-        if(!$customer->referralStat)
-            return [
-                'referral_count' => $referal_count,
-                'referral_discount' => $referal_discount
-            ];
         $referal_customer_stat = count($customer->referralStat);
         $referal_company_range = ReferralRange::where('company_id', Auth::user()->company_id)
                                                     ->where('referral_count','<=',$referal_customer_stat)
@@ -23,8 +18,10 @@ if (! function_exists('get_referral_info')) {
         } else {
             $referal_company_range = ReferralRange::where('company_id', Auth::user()->company_id)
                                                     ->first();
-            $referal_count = $referal_company_range->referral_count;
-            $referal_discount = $referal_company_range->discount;
+            if($referal_company_range){
+                $referal_count = $referal_company_range->referral_count;
+                $referal_discount = $referal_company_range->discount;
+            }
         }
 
         return [
