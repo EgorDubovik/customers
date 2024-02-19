f498cd7aead785b9fd4f3c723490c49511ea6bc7
EgorDubovik <posik.web.m@gmail.com>
2024-02-18 19:11:46 -0600

    fixes

--- app/Helpers/ReferralHelper.php
+++ app/Helpers/ReferralHelper.php
@@ -8,6 +8,11 @@ if (! function_exists('get_referral_info')) {
     {
         $referal_count = 0;
         $referal_discount = 0;
+        if(!$customer->referralStat)
+            return [
+                'referral_count' => $referal_count,
+                'referral_discount' => $referal_discount
+            ];
         $referal_customer_stat = count($customer->referralStat);
         $referal_company_range = ReferralRange::where('company_id', Auth::user()->company_id)
                                                     ->where('referral_count','<=',$referal_customer_stat)
