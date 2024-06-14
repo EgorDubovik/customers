979d7ade8af0bdca228a140dbecf28eb4f4bf406
EgorDubovik <posik.web.m@gmail.com>
2024-06-14 09:18:30 -0500

    add return phone number for non active book appointment online

---
+++ resources/views/book-appointment/react/index.blade-copy.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8"/>
+      <link rel="icon" href="https://customers.edservicetx.com/assets/images/brand/favicon.ico"/>
+      <meta name="viewport" content="width=device-width,initial-scale=1"/>
+      <meta name="theme-color" content="#000000"/>
+      <meta name="description" content="Book appointment online."/>
+      <title>Book appointment online</title>
+      <script defer="defer" src="{{ asset('assets/react/bookOnline/static/js/main.1888e3da.js') }}"></script>
+      <link href="{{ asset('assets/react/bookOnline/static/css/main.7ddc655a.css') }}" rel="stylesheet">
+   </head>
+   <body>
+      <noscript>You need to enable JavaScript to run this app.</noscript>
+      <div id="root" style="height:100%"></div>
+   </body>
+</html>
