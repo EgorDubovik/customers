cc5fef42b09520ac8eb1533d9e19d212ea4611ee
EgorDubovik <posik.web.m@gmail.com>
2024-08-12 13:50:56 -0500

    cast amount /100 on expanse

--- app/Models/Expanse.php
+++ app/Models/Expanse.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Casts\Attribute;
 class Expanse extends Model
 {
     use HasFactory;
@@ -15,4 +15,12 @@ class Expanse extends Model
         'user_id',
         'company_id',
     ];
+
+    public function amount(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => round($value/100,2),
+            set: fn($value) => round($value*100),
+        );
+    }
 }
